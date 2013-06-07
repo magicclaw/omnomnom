259a93b504b9437399817fb76f5ff527e5578441
SinisterMinister <codey.whitt@gmail.com>
2013-06-07 14:17:12 -0600

    Made root path work

--- main.js
+++ main.js
@@ -78,7 +78,12 @@ var updateHighscore = function(user, pointAdder) {
 app.listen(8085);
 
 function handler (req, res) {
-	fs.readFile(__dirname + req.url, function (err, data) {
+	var file = req.url;
+
+	if (file === '/')
+		file = '/index.html';
+
+	fs.readFile(__dirname + file, function (err, data) {
 		if (err) {
 			res.writeHead(500);
 			return res.end('Error loading file ' + req.url);
